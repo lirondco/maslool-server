8cd5b5cedcc5eedde2f5bce550a6896c88346c5d
Liron de Castro <lirondecastro@Lirons-MacBook-Air.local>
2021-03-02 08:13:59 -0800

    pre-development dependency installs

---
+++ postgrator-config.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const pg = require('pg');
+pg.defaults.ssl = process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false;
+
+module.exports = {
+  "migrationsDirectory": "migrations",
+  "driver": "pg",
+  "connectionString": (process.env.NODE_ENV === 'test')
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL,
+}
