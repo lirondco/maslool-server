bb187758433dc9d3639d2c3306c3b7d6900c52e9
Liron de Castro <lirondecastro@Lirons-MacBook-Air.local>
2021-03-10 12:17:17 -0800

    completed the auth endpoints test

--- test/auth-endpoints.spec.js
+++ test/auth-endpoints.spec.js
@@ -2,8 +2,9 @@ const knex = require('knex')
 const jwt = require('jsonwebtoken')
 const app = require('../src/app')
 const helpers = require('./test-helpers')
+const supertest = require('supertest')
 
-describe('Auth endpoints', function() {
+describe('Auth endpoints', function () {
     let db
 
     const { testUsers } = helpers.makeTrailsFixtures()
@@ -14,7 +15,7 @@ describe('Auth endpoints', function() {
             client: 'pg',
             connection: process.env.TEST_DATABASE_URL,
         })
-        app.get('db', db)
+        app.set('db', db)
     })
 
     after('disconnect from db', () => db.destroy())
@@ -46,8 +47,53 @@ describe('Auth endpoints', function() {
                     .send(loginAttemptBody)
                     .expect(400, {
                         error: `Missing '${field}' in request body`,
-                    })
+                })
             })
+        })
+
+        it(`responds 400 'invalid username or password' when bad username`, () => {
+            const invalidUser = { username: 'inexistent', password: 'existy' }
+            return supertest(app)
+                .post('/api/auth/login')
+                .send(invalidUser)
+                .expect(400, { error: `Incorrect username or password` })
+        })
+
+        it(`responds 400 'invalid username or password' when bad password`, () => {
+            const invalidPassword = { username: testUser.username, password: 'incorrect'}
+            return supertest(app)
+                .post('/api/auth/login')
+                .send(invalidPassword)
+                .expect(400, { error: 'Incorrect username or password' })
+        })
+
+        it(`responds 401 'user is banned' when the user is banned`, () => {
+            const bannedUser = { username: testUsers[2].username, password: testUsers[2].password }
+            return supertest(app)
+                .post('/api/auth/login')
+                .send(bannedUser)
+                .expect(401, { error: 'User is banned' })
+        })
+
+        it(`responds 200 and JWT auth token using secret when valid credentials`, () => {
+            const uservalidCreds = {
+                username: testUser.username,
+                password: testUser.password
+            }
+            const expectedToken = jwt.sign(
+                { user_id: testUser.id },
+                process.env.JWT_SECRET,
+                {
+                    subject: testUser.username,
+                    algorithm: 'HS256',
+                }
+            )
+            return supertest(app)
+                .post('/api/auth/login')
+                .send(uservalidCreds)
+                .expect(200, {
+                    authToken: expectedToken
+                })
         })
     })
 })
