03705114008450f848191239e5302bfeb9d0f6ec
Liron de Castro <lirondecastro@Lirons-MacBook-Air.local>
2021-03-25 09:00:29 -0700

    removed unhashed password from seed file

--- seeds/seed.maslool_tables.sql
+++ seeds/seed.maslool_tables.sql
@@ -12,7 +12,6 @@ INSERT INTO users (username, password, email, admin, banned, banned_by)
 VALUES
     (  
         'spongebob',
-        -- password: squarepants
         '$2a$12$gXYefyhXqRU5Cd0fV/dB3ulvaZmXEUZW7HMcF4ELhhUnGes64vft.',
         'sb.squarepants@bikinibottom.net',
         false,
@@ -21,7 +20,6 @@ VALUES
     ),
     (
         'patrick',
-        -- password: star
         '$2a$12$VBE1W4TbGF3pDw7806hRQOBA1zDhS9e4lMAWGw1xd6Rd7fjJPgRcC',
         'p.star@bikinibottom.net',
         false,
@@ -30,7 +28,6 @@ VALUES
     ),
     (
         'sandy',
-        -- password: texas
         '$2a$12$6tHzkU.P/ndfd8QgcbV7iutf2fJwgdcUGbBFuVg06tIhdHrUrNDWe',
         'sandy.cheeks@squirrelmail.com',
         true,
@@ -39,7 +36,6 @@ VALUES
     ),
     (
         'eugene',
-        -- password: money
         '$2a$12$1mApGq0vfeQC8to6WhPaGeqH6g8wLWBYpkqifHKaPSUVaFB5unOCG',
         'eugene.krabs@krustykrab.com',
         false,
@@ -48,7 +44,6 @@ VALUES
     ),
     (
         'liron',
-        -- password: AdminAdmin1!
         '$2a$12$dxjXi89H7AObnxh9K4AiDeHyN2bJH6I8wtdKm2HlXh7osTZY.Ltqi',
         'liron.dco@gmail.com',
         true,
@@ -57,7 +52,6 @@ VALUES
     ),
     (
         'squidward',
-        -- password: tentacles
         '$2a$12$dJEy0lZ8iXbO.y3LQLT.peLNIaA95on1BiyEPZFroS/UPkEarxO3e',
         's.tentacles@krustykrab.com',
         false,
